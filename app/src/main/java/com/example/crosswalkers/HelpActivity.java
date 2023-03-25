128f7708760241733fcfbec12eef7683c1882f70
Amy Chen <achen5@wpi.edu>
2023-03-25 11:04:47 -0400

    3/25/23 Added help screen

--- app/src/main/java/com/example/crosswalkers/HelpActivity.java
+++ app/src/main/java/com/example/crosswalkers/HelpActivity.java
@@ -3,21 +3,19 @@ package com.example.crosswalkers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class HelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_help);
     }
 
-    public void openHelpPage(View v){
-        Intent intent = new Intent(this, HelpActivity.class);
+    public void closeHelpPage(View v){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
