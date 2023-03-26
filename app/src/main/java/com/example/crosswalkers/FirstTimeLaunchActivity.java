1b28aedf81e42c09ced49e831384eeb41a88e726
Amy Chen <achen5@wpi.edu>
2023-03-26 19:23:52 -0400

    3/26 Added first time launch screen & tweaked the help body text

---
+++ app/src/main/java/com/example/crosswalkers/FirstTimeLaunchActivity.java
@@ -0,0 +1,42 @@
+package com.example.crosswalkers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+
+public class FirstTimeLaunchActivity extends AppCompatActivity {
+
+    String prevStarted = "yes";
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedpreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+        if (!sharedpreferences.getBoolean(prevStarted, false)) {
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putBoolean(prevStarted, Boolean.TRUE);
+            editor.apply();
+        } else {
+            goToMain();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time_launch);
+    }
+
+    public void goToMain(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void closeLaunchPage(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
