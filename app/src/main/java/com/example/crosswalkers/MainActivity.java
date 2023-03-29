1f87c0b08591103190a56fb1885a7388c4d3d448
Amy Chen <achen5@wpi.edu>
2023-03-29 13:18:46 -0400

    3/29/23 Implemented Camera X take photo function

--- app/src/main/java/com/example/crosswalkers/MainActivity.java
+++ app/src/main/java/com/example/crosswalkers/MainActivity.java
@@ -1,23 +1,132 @@
 package com.example.crosswalkers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    PreviewView previewView;
+    Button bTakePicture, bRecording;
+    private ImageCapture imageCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bTakePicture = findViewById(R.id.bTakePicture);
+        bRecording = findViewById(R.id.bRecording);
+        previewView = findViewById(R.id.previewView);
+
+        bTakePicture.setOnClickListener(this);
+        bRecording.setOnClickListener(this);
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCameraX(cameraProvider);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, getExecutor());
     }
 
+    private Executor getExecutor(){
+        return ContextCompat.getMainExecutor(this);
+    }
+
+    private void startCameraX(ProcessCameraProvider cameraProvider){
+        cameraProvider.unbindAll();
+
+        //Camera Selector use case
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+
+        //Preview use case
+        Preview preview = new Preview.Builder().build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        //Image capture use case
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY).build();
+
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture);
+    }
+    @Override
+    public void onClick(View v){
+       switch(v.getId()){
+           case R.id.bTakePicture:
+               capturePhoto();
+               break;
+           case R.id.bRecording:
+               break;
+       }
+    }
+
+    private void capturePhoto(){
+
+
+        long timestamp = System.currentTimeMillis();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timestamp);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+
+
+        imageCapture.takePicture(
+                new ImageCapture.OutputFileOptions.Builder(getContentResolver(), MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues).build(),
+                getExecutor(),
+                new ImageCapture.OnImageSavedCallback(){
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults){
+                        Toast.makeText(MainActivity.this, "Photo has been saved successfully.",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception){
+                        Toast.makeText(MainActivity.this, "Error saving photo: " + exception.getMessage(),Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+        );
+    }
     public void openHelpPage(View v){
         Intent intent = new Intent(this, HelpActivity.class);
         startActivity(intent);
     }
 }
+
+// q's for Mrs. Taricco
+// why need kotlin? (gradle build)
+//
