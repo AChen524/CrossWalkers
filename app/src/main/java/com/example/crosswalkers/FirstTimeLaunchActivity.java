8bd886f08488779f0ec36c71aec31f87706603f5
Amy Chen <achen5@wpi.edu>
2023-04-04 12:14:15 -0400

    4/4/23 Tweaked permissions

--- app/src/main/java/com/example/crosswalkers/FirstTimeLaunchActivity.java
+++ app/src/main/java/com/example/crosswalkers/FirstTimeLaunchActivity.java
@@ -19,18 +19,13 @@ import android.widget.Toast;
 
 public class FirstTimeLaunchActivity extends AppCompatActivity {
     private int CAMERA_PERMISSION_CODE = 1;
-
-    String prevStarted = "yes";
+    private String permissionGranted = "permission granted";
 
     @Override
     protected void onResume() {
         super.onResume();
-        SharedPreferences sharedpreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
-        if (!sharedpreferences.getBoolean(prevStarted, false)) {
-            SharedPreferences.Editor editor = sharedpreferences.edit();
-            editor.putBoolean(prevStarted, Boolean.TRUE);
-            editor.apply();
-        } else {
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+        if (sharedPreferences.getBoolean(permissionGranted, false)==true) {
             goToMain();
         }
     }
@@ -40,14 +35,30 @@ public class FirstTimeLaunchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_time_launch);
 
+
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+        if (sharedPreferences.getBoolean(permissionGranted, false)==true) {
+            goToMain();
+        }
+        else {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(permissionGranted, Boolean.FALSE);
+            editor.apply();
+        }
+
         Button requestPermission = findViewById(R.id.continueButton);
         requestPermission.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (ContextCompat.checkSelfPermission(FirstTimeLaunchActivity.this,
                         android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(FirstTimeLaunchActivity.this, "You have already granted this permission", Toast.LENGTH_SHORT).show();
-                } else {
+                    SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean(permissionGranted, Boolean.TRUE);
+                    editor.apply();
+                    goToMain();
+                }
+                else {
                     requestCameraPermissions();
                 }
             }
@@ -68,12 +79,24 @@ public class FirstTimeLaunchActivity extends AppCompatActivity {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
                             ActivityCompat.requestPermissions(FirstTimeLaunchActivity.this,new String[]{android.Manifest.permission.CAMERA},CAMERA_PERMISSION_CODE);
+                            if (ContextCompat.checkSelfPermission(FirstTimeLaunchActivity.this,
+                                    android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                                Toast.makeText(FirstTimeLaunchActivity.this, "You have already granted this permission", Toast.LENGTH_SHORT).show();
+                                SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putBoolean(permissionGranted, Boolean.TRUE);
+                                editor.apply();
+                                goToMain();
+                            }
+                            else {
+                                requestCameraPermissions();
+                            }
                         }
                     })
                     .setNegativeButton("cancel", new DialogInterface.OnClickListener(){
                         @Override
                         public void onClick(DialogInterface dialog, int i){
-                            dialog.dismiss();
+                        dialog.dismiss();
                         }
                  })
                     .create().show();
@@ -87,10 +110,15 @@ public class FirstTimeLaunchActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);//why needed?
         if (requestCode == CAMERA_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) ;
-            Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT);
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(FirstTimeLaunchActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean(permissionGranted, Boolean.TRUE);
+                editor.apply();
+            }
         } else {
-            Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT);
+            Toast.makeText(FirstTimeLaunchActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
         }
     }
 
